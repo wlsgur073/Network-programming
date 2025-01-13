70a361c1f3137fbedf690025caccc3d225b75bb5
wlsgur073 <wlsgur073@gmail.com>
2025-01-13 19:27:39 +0900

    The way it works is very similar to `EtherSample`, it opens a pcap file and reads the trace.

---
+++ NpcapSamples/01_PcapFileSample/01_PcapFileSample.cpp
@@ -0,0 +1,100 @@
+﻿#include <stdio.h>
+#include <pcap.h>
+#include <time.h>
+
+#pragma comment(lib, "wpcap")
+#pragma comment(lib, "ws2_32")
+
+#include <tchar.h>
+#include <WinSock2.h>
+
+#pragma pack(push, 1)
+typedef struct EtherHeader {
+	unsigned char dstMac[6];
+	unsigned char srcMac[6];
+	unsigned short type;
+} EtherHeader;
+#pragma pack(pop)
+
+BOOL LoadNpcapDlls()
+{
+	_TCHAR npcap_dir[512];
+	UINT len;
+	len = GetSystemDirectory(npcap_dir, 480);
+	if (!len) {
+		fprintf(stderr, "Error in GetSystemDirectory: %x", GetLastError());
+		return FALSE;
+	}
+	_tcscat_s(npcap_dir, 512, _T("\\Npcap"));
+	if (SetDllDirectory(npcap_dir) == 0) {
+		fprintf(stderr, "Error in SetDllDirectory: %x", GetLastError());
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+#define LINE_LEN 16
+
+void dispatcher_handler(u_char* temp1,
+	const struct pcap_pkthdr* header,
+	const u_char* pkt_data)
+{
+	u_int i = 0;
+
+	/* print pkt timestamp and pkt len */
+	printf("%ld:%ld (%ld)\n", header->ts.tv_sec, header->ts.tv_usec, header->len);
+
+	EtherHeader* pEther = (EtherHeader*)pkt_data;
+
+	printf(
+		"SRC: %02X-%02X-%02X-%02X-%02X-%02X -> "
+		"DST: %02X-%02X-%02X-%02X-%02X-%02X, type:%04X\n",
+		pEther->srcMac[0], pEther->srcMac[1], pEther->srcMac[2],
+		pEther->srcMac[3], pEther->srcMac[4], pEther->srcMac[5],
+		pEther->dstMac[0], pEther->dstMac[1], pEther->dstMac[2],
+		pEther->dstMac[3], pEther->dstMac[4], pEther->dstMac[5],
+		ntohs(pEther->type)
+	);
+
+	/* Print the packet */
+	for (i = 1; (i < header->caplen + 1); i++)
+	{
+		printf("%.2x ", pkt_data[i - 1]);
+		if ((i % LINE_LEN) == 0) printf("\n");
+	}
+
+	printf("\n\n");
+}
+
+int main(int argc, char** argv)
+{
+	pcap_t* fp;
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	if (!LoadNpcapDlls())
+	{
+		fprintf(stderr, "Couldn't load Npcap\n");
+		exit(1);
+	}
+
+	/* Open the capture file */
+	if ((fp = pcap_open_offline(
+		"C:\\SampleTraces\\ip-fragments.pcap",
+		errbuf)) == NULL)
+	{
+		fprintf(stderr, "\nUnable to open the file %s.\n",
+			"C:\\SampleTraces\\ip-fragments.pcap");
+		return -1;
+	}
+
+	/* read and dispatch packets until EOF is reached */
+	pcap_loop(fp, 0, dispatcher_handler, NULL);
+
+	pcap_close(fp);
+	return 0;
+}
+
+
+
+
