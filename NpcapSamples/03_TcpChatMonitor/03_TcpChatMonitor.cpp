8f7335efd3ef368c38585817345717faa47b2789
wlsgur073 <wlsgur073@gmail.com>
2025-01-15 20:44:49 +0900

    If the packets are not encrypted, they can be parsed and viewed.

---
+++ NpcapSamples/03_TcpChatMonitor/03_TcpChatMonitor.cpp
@@ -0,0 +1,188 @@
+﻿#include <stdio.h>
+#include <pcap.h>
+#include <time.h>
+
+#pragma comment(lib, "wpcap")
+#pragma comment(lib, "ws2_32")
+
+#include <tchar.h>
+#include <WinSock2.h>
+
+#pragma pack(push, 1)
+typedef struct EtherHeader {
+	unsigned char dstMac[6];
+	unsigned char srcMac[6];
+	unsigned short type;
+} EtherHeader;
+
+typedef struct IpHeader {
+	unsigned char verIhl;
+	unsigned char tos;
+	unsigned short length;
+	unsigned short id;
+	unsigned short fragOffset;
+	unsigned char ttl;
+	unsigned char protocol;
+	unsigned short checksum;
+	unsigned char srcIp[4];
+	unsigned char dstIp[4];
+} IpHeader;
+
+typedef struct TcpHeader {
+	unsigned short srcPort;
+	unsigned short dstPort;
+	unsigned int seq;
+	unsigned int ack;
+	unsigned char data;
+	unsigned char flags;
+	unsigned short windowSize;
+	unsigned short checksum;
+	unsigned short urgent;
+} TcpHeader;
+#pragma pack(pop)
+
+BOOL LoadNpcapDlls()
+{
+	_TCHAR npcap_dir[512];
+	UINT len;
+	len = GetSystemDirectory(npcap_dir, 480);
+	if (!len) {
+		fprintf(stderr, "Error in GetSystemDirectory: %x", GetLastError());
+		return FALSE;
+	}
+	_tcscat_s(npcap_dir, 512, _T("\\Npcap"));
+	if (SetDllDirectory(npcap_dir) == 0) {
+		fprintf(stderr, "Error in SetDllDirectory: %x", GetLastError());
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+void packet_handler(u_char* temp1,
+	const struct pcap_pkthdr* header,
+	const u_char* pkt_data)
+{
+	EtherHeader* pEther = (EtherHeader*)pkt_data;
+	IpHeader* pIpHeader = (IpHeader*)(pkt_data + sizeof(EtherHeader));
+
+	if (pEther->type != 0x0008)
+		return;
+
+	if (pIpHeader->protocol != 6)
+		return;
+
+	int ipHeaderLen = (pIpHeader->verIhl & 0x0F) * 4;
+	TcpHeader* pTcp =
+		(TcpHeader*)(pkt_data + sizeof(EtherHeader) + ipHeaderLen);
+
+	if (ntohs(pTcp->srcPort) != 25000 &&
+		ntohs(pTcp->dstPort) != 25000)
+		return;
+
+	printf("%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d\n",
+		pIpHeader->srcIp[0], pIpHeader->srcIp[1],
+		pIpHeader->srcIp[2], pIpHeader->srcIp[3],
+		ntohs(pTcp->srcPort),
+		pIpHeader->dstIp[0], pIpHeader->dstIp[1],
+		pIpHeader->dstIp[2], pIpHeader->dstIp[3],
+		ntohs(pTcp->dstPort)
+	);
+
+	// TCP header size is 4bit of data field
+	int tcpHeaderSize = ((pTcp->data >> 4 & 0x0F) * 4); // Data(4bit) >> 4 and clear 4bit to prevent unsigned overflow
+	char* pPayload = (char*)(pkt_data + sizeof(EtherHeader) +
+		ipHeaderLen + tcpHeaderSize);
+	
+	printf("Segment size: %d(Frame length: %d)\n",
+		ntohs(pIpHeader->length) - ipHeaderLen - tcpHeaderSize,
+		header->len);
+
+	char szMessage[2048] = { 0 }; // store enough size more than 1514 bytes of Ethernet frame
+	memcpy_s(szMessage, sizeof(szMessage), pPayload,
+		ntohs(pIpHeader->length) - ipHeaderLen - tcpHeaderSize);
+	puts(szMessage);
+}
+
+int main()
+{
+	pcap_if_t* alldevs;
+	pcap_if_t* d;
+	int inum;
+	int i = 0;
+	pcap_t* adhandle;
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	if (!LoadNpcapDlls())
+	{
+		fprintf(stderr, "Couldn't load Npcap\n");
+		exit(1);
+	}
+
+
+	/* Retrieve the device list */
+	if (pcap_findalldevs(&alldevs, errbuf) == -1)
+	{
+		fprintf(stderr, "Error in pcap_findalldevs: %s\n", errbuf);
+		exit(1);
+	}
+
+	/* Print the list */
+	for (d = alldevs; d; d = d->next)
+	{
+		printf("%d. %s", ++i, d->name);
+		if (d->description)
+			printf(" (%s)\n", d->description);
+		else
+			printf(" (No description available)\n");
+	}
+
+	if (i == 0)
+	{
+		printf("\nNo interfaces found! Make sure Npcap is installed.\n");
+		return -1;
+	}
+
+	printf("Enter the interface number (1-%d):", i);
+	scanf_s("%d", &inum);
+
+	if (inum < 1 || inum > i)
+	{
+		printf("\nInterface number out of range.\n");
+		/* Free the device list */
+		pcap_freealldevs(alldevs);
+		return -1;
+	}
+
+	/* Jump to the selected adapter */
+	for (d = alldevs, i = 0; i < inum - 1; d = d->next, i++);
+
+	/* Open the device */
+	/* Open the adapter */
+	if ((adhandle = pcap_open_live(d->name,	// name of the device
+		65536,			// portion of the packet to capture. 
+		// 65536 grants that the whole packet will be captured on all the MACs.
+		1,				// promiscuous mode (nonzero means promiscuous)
+		1000,			// read timeout
+		errbuf			// error buffer
+	)) == NULL)
+	{
+		fprintf(stderr, "\nUnable to open the adapter. %s is not supported by Npcap\n", d->name);
+		/* Free the device list */
+		pcap_freealldevs(alldevs);
+		return -1;
+	}
+
+	printf("\nlistening on %s...\n", d->description);
+
+	/* At this point, we don't need any more the device list. Free it */
+	pcap_freealldevs(alldevs);
+
+	/* start the capture */
+	pcap_loop(adhandle, 0, packet_handler, NULL);
+
+	pcap_close(adhandle);
+
+	return 0;
+}
