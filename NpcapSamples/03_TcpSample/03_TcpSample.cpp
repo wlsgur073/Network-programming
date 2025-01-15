b3f2ebaf3696f16bff4e3b1795d02ac35a9b1fb9
wlsgur073 <wlsgur073@gmail.com>
2025-01-15 18:02:06 +0900

    We learned in the previous lesson that casting pkt_data to EtherHeader initially yields EtherHeader, and now we know that the address jumped by sizeof(EtherHeader) is IpHeader.
    TcpHeader can get the IHL by multiplying pIpHeader by verIhl(version+IHL) times 4. Multiplying by 4 because it is IPv4 based.
    
    Since there is no 4-bit data type in C, I learned that two 4-bit pieces of information can be combined into an 8-bit representation. In other words, verIhl contains the Version and IHL of the IpHeader.

---
+++ NpcapSamples/03_TcpSample/03_TcpSample.cpp
@@ -0,0 +1,128 @@
+﻿#include <stdio.h>
+#include <pcap.h>
+#include <time.h>
+
+#pragma comment(lib, "wpcap")
+#pragma comment(lib, "ws2_32")
+
+#include <tchar.h>
+#include <WinSock2.h>
+
+#pragma pack(push, 1)
+typedef struct EtherHeader {
+	unsigned char dstMac[6];
+	unsigned char srcMac[6];
+	unsigned short type;
+} EtherHeader;
+
+typedef struct IpHeader {
+	unsigned char verIhl;
+	unsigned char tos;
+	unsigned short length;
+	unsigned short id;
+	unsigned short fragOffset;
+	unsigned char ttl;
+	unsigned char protocol;
+	unsigned short checksum;
+	unsigned char srcIp[4];
+	unsigned char dstIp[4];
+} IpHeader;
+
+typedef struct TcpHeader {
+	unsigned short srcPort;
+	unsigned short dstPort;
+	unsigned int seq;
+	unsigned int ack;
+	unsigned char data;
+	unsigned char flags;
+	unsigned short windowSize;
+	unsigned short checksum;
+	unsigned short urgent;
+} TcpHeader;
+#pragma pack(pop)
+
+BOOL LoadNpcapDlls()
+{
+	_TCHAR npcap_dir[512];
+	UINT len;
+	len = GetSystemDirectory(npcap_dir, 480);
+	if (!len) {
+		fprintf(stderr, "Error in GetSystemDirectory: %x", GetLastError());
+		return FALSE;
+	}
+	_tcscat_s(npcap_dir, 512, _T("\\Npcap"));
+	if (SetDllDirectory(npcap_dir) == 0) {
+		fprintf(stderr, "Error in SetDllDirectory: %x", GetLastError());
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+void dispatcher_handler(u_char* temp1,
+	const struct pcap_pkthdr* header,
+	const u_char* pkt_data)
+{
+	EtherHeader* pEther = (EtherHeader*)pkt_data; // cast to EtherHeader
+	IpHeader* pIpHeader = (IpHeader*)(pkt_data + sizeof(EtherHeader)); // you can find the IP header jumping over the Ethernet header size
+
+	if (pEther->type != 0x0008)
+		return;
+
+	if (pIpHeader->protocol != 6)
+		return;
+
+	int ipLen = (pIpHeader->verIhl & 0x0F) * 4; // get the IHL(IP header length), std. IPv4 header is 20 bytes
+	TcpHeader* pTcp =
+		(TcpHeader*)(pkt_data + sizeof(EtherHeader) + ipLen);
+
+	printf("%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d\n",
+		pIpHeader->srcIp[0], pIpHeader->srcIp[1],
+		pIpHeader->srcIp[2], pIpHeader->srcIp[3],
+		ntohs(pTcp->srcPort),
+		pIpHeader->dstIp[0], pIpHeader->dstIp[1],
+		pIpHeader->dstIp[2], pIpHeader->dstIp[3],
+		ntohs(pTcp->dstPort)
+	);
+
+	// if all of flags are 1, it means XMAS scan, unusual!
+	if (pTcp->flags == 0x02)
+		puts("SYN");
+	else if (pTcp->flags == 0x12)
+		puts("SYN + ACK");
+	else if (pTcp->flags == 0x10)
+		puts("ACK");
+
+	if (pTcp->flags & 0x04) // 0000 0100
+		puts("*RST");
+}
+
+int main(int argc, char** argv)
+{
+	pcap_t* fp;
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	if (!LoadNpcapDlls())
+	{
+		fprintf(stderr, "Couldn't load Npcap\n");
+		exit(1);
+	}
+
+	/* Open the capture file */
+	if ((fp = pcap_open_offline(
+		"C:\\SampleTraces\\http-browse-ok.pcap",
+		errbuf)) == NULL)
+	{
+		fprintf(stderr, "\nUnable to open the file %s.\n",
+			"C:\\SampleTraces\\http-browse-ok.pcap");
+		return -1;
+	}
+
+	/* read and dispatch packets until EOF is reached */
+	pcap_loop(fp, 0, dispatcher_handler, NULL);
+
+	pcap_close(fp);
+	return 0;
+}
+
